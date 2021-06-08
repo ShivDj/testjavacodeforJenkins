063ad1fdb85ccad08b0c98fcfa1a383a38542b71
ShivDj <62321737+ShivDj@users.noreply.github.com>
2021-06-08 13:48:07 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld
 public static void main(String args[])   
 {  
 int number=100;  
+System.out.print("Sheevendra Singh"); 
 System.out.print("List of even numbers from 1 to "+number+": ");  
 for (int i=1; i<=number; i++)   
 {    
