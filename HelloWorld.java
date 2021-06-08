0bdc19c55d291fad5ec4b8873cf7ffb51dad1ad7
ShivDj <62321737+ShivDj@users.noreply.github.com>
2021-06-08 13:12:15 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,15 @@
-class HelloWorld {
-    public static void main( String []args ) {
-        System.out.println( "Hello World!" );
-    }
-}
+public class DisplayEvenNumbersExample1  
+{  
+public static void main(String args[])   
+{  
+int number=100;  
+System.out.print("List of even numbers from 1 to "+number+": ");  
+for (int i=1; i<=number; i++)   
+{    
+if (i%2==0)   
+{  
+System.out.print(i + " ");  
+}  
+}  
+}  
+}  
